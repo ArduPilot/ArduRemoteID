25f4d015b46b4ad8c0de96432236a32f9d0925ac
Andrew Tridgell <andrew@tridgell.net>
2022-08-12 17:11:38 +1000

    added WiFi NAN driver

---
+++ RemoteIDModule/WiFi_TX.h
@@ -0,0 +1,20 @@
+/*
+  WiFi beacon driver
+ */
+#pragma once
+
+#include <Arduino.h>
+#include <opendroneid.h>
+
+class WiFi_NAN {
+public:
+    bool init(const char *ssid);
+    bool transmit(ODID_UAS_Data &UAS_data);
+
+private:
+    char ssid[32];
+    uint8_t WiFi_mac_addr[6];
+    uint8_t wifi_channel = 6;
+    size_t ssid_length;
+    uint8_t send_counter;
+};
