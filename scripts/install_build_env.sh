bd7e12775681d5d260f45fa03b084c30a0b315df
Soren Friis <soren.friis@intel.com>
2022-08-16 10:38:56 +0300

    Add pyserial to environment dependencies
    Signed-off-by: Soren Friis <soren.friis@intel.com>

--- scripts/install_build_env.sh
+++ scripts/install_build_env.sh
@@ -3,6 +3,7 @@
 python3 -m pip install empy
 python3 -m pip install pymavlink
 python3 -m pip install dronecan
+python3 -m pip install pyserial
 wget https://downloads.arduino.cc/arduino-cli/arduino-cli_0.26.0_Linux_64bit.tar.gz
 mkdir -p bin
 (cd bin && tar xvzf ../arduino-cli_0.26.0_Linux_64bit.tar.gz)
