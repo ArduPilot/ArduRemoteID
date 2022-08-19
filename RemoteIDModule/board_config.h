7591cc50f796601d63c38c4a32456f7b2bc4d1bb
Roel Schiphorst <roel@bluemark.io>
2022-08-19 16:21:36 +0200

    BlueMark db200: add LED status and set ESP GPIO pins to enable CAN transceiver chip

--- RemoteIDModule/board_config.h
+++ RemoteIDModule/board_config.h
@@ -19,11 +19,20 @@
 #define PIN_UART_RX 2
 
 #elif defined(BOARD_BLUEMARK_DB200)
-#define PIN_CAN_TX GPIO_NUM_5
-#define PIN_CAN_RX GPIO_NUM_4
+
+#define PIN_CAN_TX GPIO_NUM_0 // this goes to the TX pin (= input) of the NXP CAN transceiver
+#define PIN_CAN_RX GPIO_NUM_5
+#define PIN_CAN_EN GPIO_NUM_4 // EN pin of the NXP CAN transceiver
+#define PIN_CAN_nSILENT GPIO_NUM_1 // CAN silent pin, active low
+
+// to enable termination resistors uncomment this line
+//#define PIN_CAN_TERM EN_NUM_10 // if set to ON, the termination resistors of the CAN bus are enabled
 
 #define PIN_UART_TX 3
 #define PIN_UART_RX 2
+
+#define PIN_STATUS_LED GPIO_NUM_8 // LED to signal the status
+#define STATUS_LED_ON 1
 
 #elif defined(BOARD_BLUEMARK_DB110)
 #define PIN_UART_TX 5
