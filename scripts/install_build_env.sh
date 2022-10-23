ae0942c48c0b9542723864b6303022366287fcd3
Dan Lynch <92399216+DanLynch-AR@users.noreply.github.com>
2022-10-23 12:05:18 -0400

    fix: Install `pexpect` to fix failing `regen_headers.sh`
    While following the linux build directions in `BUILD.md`, the script `scripts/regen_headers.sh` fails to generate DroneCAN headers due to a missing python module:
    ```
    Generating DroneCAN headers for libcanard
    Traceback (most recent call last):
      File "/flash/arduremoteid/modules/dronecan_dsdlc/dronecan_dsdlc.py", line 20, in <module>
        from dronecan_dsdlc_tester import *
      File "/flash/arduremoteid/modules/dronecan_dsdlc/dronecan_dsdlc_tester.py", line 12, in <module>
        import pexpect
    ModuleNotFoundError: No module named 'pexpect'
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ./scripts/regen_headers.sh: line 16: cd: libraries/DroneCAN_generated: No such file or directory
    ```
    
    This issue is resolved (and build completed successfully) following a `pip install pexpect`.

--- scripts/install_build_env.sh
+++ scripts/install_build_env.sh
@@ -4,6 +4,7 @@ python3 -m pip install empy
 python3 -m pip install pymavlink
 python3 -m pip install dronecan
 python3 -m pip install pyserial
+python3 -m pip install pexpect
 wget https://downloads.arduino.cc/arduino-cli/arduino-cli_0.27.1_Linux_64bit.tar.gz
 mkdir -p bin
 (cd bin && tar xvzf ../arduino-cli_0.27.1_Linux_64bit.tar.gz)
